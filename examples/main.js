c6056d32fe3a08d0fe3f7712acf0608c416f2b69
goliatone <burgosemi@gmail.com>
2014-03-15 23:14:50 -0400

    Example: Use jQuery.

--- examples/main.js
+++ examples/main.js
@@ -3,11 +3,12 @@
 requirejs.config({
     paths: {
         'user': 'user',
-        'gpub':'components/gpub/gpub'
+        'gpub':'components/gpub/gpub',
+        'jquery':'jquery/jquery'
     }
 });
 
-define(['gmodel', 'gpub'], function (Gmodel, Gpub) {
+define(['gmodel', 'gpub','jquery'], function (Gmodel, Gpub, $) {
     console.log('Loading');
     var config = {data:{name:'goliatone', age:32}};
 	var user = new Gmodel(config);
@@ -17,37 +18,16 @@ define(['gmodel', 'gpub'], function (Gmodel, Gpub) {
 
 	var handleChanges = function handleChanges(event){
 		console.log('Model udpate', event.property, 'from', event.old, 'to', event.value);
-		$('.'+ event.property,'#'+ user.get('id')).innherHTML = event.value;
+		$('.'+ event.property, '#'+ user.get('id')).html(event.value);
 	};
 
 	user.on('change', handleChanges);
 
 	user.format('name', function(name){ return name+'!'; });
-	
+
 	user.set('id', 'user');
 	user.set('name', 'Goliatone');
 	user.set('age', 33);
 
 	window.user = user;
 });
-
-var $ = function(
-  a,                         // take a simple selector like "name", "#name", or ".name", and
-  b                          // an optional context, and
-){
-  a = a.match(/^(\W)?(.*)/); // split the selector into name and symbol.
-  return(                    // return an element or list, from within the scope of
-    b                        // the passed context
-    || document              // or document,
-  )[
-    "getElement" + (         // obtained by the appropriate method calculated by
-      a[1]
-        ? a[1] == "#"
-          ? "ById"           // the node by ID,
-          : "sByClassName"   // the nodes by class name, or
-        : "sByTagName"       // the nodes by tag name,
-    )
-  ](
-    a[2]                     // called with the name.
-  )
-}
